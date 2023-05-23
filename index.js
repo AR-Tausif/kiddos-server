db8591b3a651be1e483fa22c083c29da82f00dff
AR-Tausif <tausif.ritu1@gmail.com>
2023-05-23 23:20:56 +0600

    search emplimentation sedone

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const app = express();
 const port = 3500;
@@ -24,13 +24,13 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     // Send a ping to confirm a successful connection
 
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
+    // await client.db("admin").command({ ping: 1 });
+    // console.log(
+    //   "Pinged your deployment. You successfully connected to MongoDB!"
+    // );
 
     const database = client.db("kiddosProducts").collection("products");
 
@@ -40,8 +40,54 @@ async function run() {
       const result = await database.insertOne(body);
       res.send(result);
     });
+
+    // const query = {};
+            // const query = { price: {$gte: 50, $lte:150}};
+            // db.InspirationalWomen.find({first_name: { $regex: /Harriet/i} })
+
+       // const options = {
+            //     // sort matched documents in descending order by rating
+            //     sort: { 
+            //         "price": sort === 'asc' ? 1 : -1
+            //     }
+                
+            // };
+    // addedToys
+    // app.get("/addedToys", async (req, res) => {
+    //   console.log(req.query.email);
+    //   let query = {};
+    //   if (req.query?.email) {
+    //     query = { sellerEmail: req.query.email };
+    //   }
+    //   if (req.query.search) {
+    //     query = {
+    //       ...query,
+    //       productName: { $regex: req.query.search, $options: "i" },
+    //     };
+    //   }
+    //   const cursor = addedToys
+    //     .find(query)
+    //     .limit(parseFloat(req.query.limit || 20))
+    //     .sort({
+    //       price: req.query.sort === "acc" ? 1 : -1,
+    //     });
+    //   const result = await cursor.toArray();
+    //   return res.send(result);
+    // });
+
     app.get("/products", async (req, res) => {
-      const cursor = database.find();
+      const sort = req.query.sort;
+      const search = req.query.search;
+      console.log(search);
+      let query = {}
+      if(search){
+        query = {
+          ...query,
+          name: { $regex: search, $options: 'i'}
+        }
+      }
+            
+      const cursor = database.find(query);
       const result = await cursor.toArray();
       res.send(result);
     });
@@ -52,6 +98,15 @@ async function run() {
       const result = await cursor.toArray();
       res.send(result);
     });
+    app.delete("/products/email", async (req, res) => {
+        const id = req.query.id;
+        console.log(id)
+      const query = { _id: new ObjectId(id) };
+      const result = database.deleteOne(query);
+      res.send(result);
+    });
+
+
     // app.get("/products", async (req, res) => {
     //   console.log(req.query.email);
     //   const query = { seller_email: req.query.email };
