0e5d8722abd5f4b94ba12230c62af26e1c69192f
AR-Tausif <tausif.ritu1@gmail.com>
2023-05-23 23:32:53 +0600

    delete method done

--- index.js
+++ index.js
@@ -98,7 +98,7 @@ async function run() {
       const result = await cursor.toArray();
       res.send(result);
     });
-    app.delete("/products/email", async (req, res) => {
+    app.delete("/products", async (req, res) => {
         const id = req.query.id;
         console.log(id)
       const query = { _id: new ObjectId(id) };
